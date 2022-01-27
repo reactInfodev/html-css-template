91ec7ba5a3161d606b957a2a9bb6b7664e307810
Sajan Nepali <sajan@Sajans-MacBook-Air.local>
2022-01-27 20:13:45 +0545

    completed hero section

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,10 @@ module.exports = {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader"
+            loader: "html-loader",
+            options: {
+              sources:true
+            }
           }
         ]
       },
